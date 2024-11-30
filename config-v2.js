c8de89f11370602fdb6b14a3c70e9e58b61838cb
Mysl835 <hsboy5014@gmail.com>
2024-11-30 13:26:38 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94711814650"
+const SESSION_ID = "PRABATH-MD~OJVTnLbD#j3F4j1ll6I4UYh_3e2karMkFl0Zuu8a5ShtBbPopnVQ"
 
 
 module.exports = {
